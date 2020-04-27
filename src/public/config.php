c6f3964bb53d076f34f510937b3d0c5c3f8c1ddd
Sven Maack <sven.maack@gmail.com>
2020-04-27 08:46:48 +0200

    using correct config

--- src/public/config.php
+++ src/public/config.php
@@ -1,10 +1,34 @@
-<?php
-return [
-    'db' => [
-        'username' => '%db.user%',
-        'password' => '%db.password%',
-        'dbname' => '%db.database%',
-        'host' => '%db.host%',
-        'port' => '%db.port%'
-    ]
+<?php return [
+    'db' =>
+        [
+            'host' => getenv('MYSQL_HOST'),
+            'port' => '3306',
+            'username' => getenv('MYSQL_USER'),
+            'password' => getenv('MYSQL_PASSWORD'),
+            'dbname' => getenv('MYSQL_DATABASE'),
+        ],
+    'csrfProtection' => [
+        'frontend' => false,
+        'backend' => false
+    ],
+    'phpsettings' => [
+        'display_errors' => 1,
+        'session.gc_maxlifetime' => 7200
+    ],
+    'front' => [
+        'throwExceptions' => true,
+    ],
+    'template' => [
+        'forceCompile' => true,
+    ],
+    'httpcache' => [
+        'enabled' => false,
+        'debug' => true,
+    ],
+    'cache' => [
+        'backend' => 'Black-Hole'
+    ],
+    'model' => [
+        'cacheProvider' => 'Array'
+    ],
 ];
